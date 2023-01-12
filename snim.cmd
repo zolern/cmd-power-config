e16ae26683213eef5a867f3d21c16a96a8d6cf05
Encho "zolern" Topalov <entopalov@gmail.com>
2023-01-12 12:27:49 +0200

    update snim command

--- snim.cmd
+++ snim.cmd
@@ -13,6 +13,13 @@
 @pushd .
 @call d snim
 @rd now > NUL 2> NUL
-@mklink /j now nim-%1 > NUL
+if exist %1 (
+	@mklink /j now %1 > NUL
+	goto :next
+)
+if exist nim-%1 (
+	@mklink /j now nim-%1 > NUL
+)
+:next
 @nim -v
 @popd
